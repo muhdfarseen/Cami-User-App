a5577d231685cc5ed93e6ca0b97206a76bc1063b
Farseen <muhdfarseen@gmail.com>
2024-02-11 23:06:20 +0530

    feat: message receive

--- src/TabComponents/Messages.jsx
+++ src/TabComponents/Messages.jsx
@@ -1,43 +1,65 @@
-import React from 'react'
-import { Image, rem, Badge, Center, Group, SegmentedControl, Card, Title, BackgroundImage, Alert, PinInput, NumberInput, Modal, Select, Flex, Mark, Grid, Text, TextInput, Button, PasswordInput, Box } from '@mantine/core';
-import { IconSend, IconChecks, IconTrash, IconMessagePlus, IconMessageCheck } from '@tabler/icons-react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Box, Grid, Card, Text, Badge, Title } from '@mantine/core';
 
 function Messages() {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const [messages, setMessages] = useState([]);
 
-  const notifications = Array.from({ length: 10 }, (_, index) => index + 1);
+  useEffect(() => {
+    const studentDataString = localStorage.getItem('studentData');
+    const studentData = JSON.parse(studentDataString);
 
+    if (studentData && studentData.admission_year && studentData.department) {
+      const admissionYear = studentData.admission_year;
+      const department = studentData.department;
+
+      const fetchMessages = async () => {
+        try {
+          const response = await axios.get(`${baseUrl}/delivered-messages/${admissionYear}/${department}`);
+          setMessages(response.data);
+        } catch (error) {
+          console.error('Error fetching messages:', error);
+        }
+      };
+
+      fetchMessages();
+
+    } else {
+      console.log('Admission year or department is missing in local storage');
+    }
+  }, []);
+
+
+  const formatTime = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  };
 
   return (
     <div>
       <Box p={30}>
-
         <Title order={2}>Messages</Title>
-
         <Grid mt={20} pb={70}>
-          {notifications.map((notification) => (
-            <Grid.Col key={notification} span={{ base: 12, md: 6, lg: 3 }}>
+          {messages.map((message) => (
+            <Grid.Col key={message.msg_id} span={{ base: 12, md: 6, lg: 3 }}>
               <Card withBorder radius="md">
                 <Text mb="xs" fw={500}>
-                  No Bus Tomorrow
+                  {message.subject}
                 </Text>
                 <Text size="sm" c="dimmed">
-                  Due to unforeseen circumstances, there will be no bus service tomorrow. We apologize for any inconvenience caused.
+                  {message.description}
                 </Text>
-
-                <Badge mt={10} leftSection={<IconChecks style={{ width: rem(12), height: rem(12) }} />} variant="outline" color="gray">
-                  12 Jan 23
+                <Badge mt={10} variant="outline" color="gray">
+                  {formatTime(message.sent_time)}
                 </Badge>
-
               </Card>
             </Grid.Col>
           ))}
         </Grid>
-
       </Box>
-
     </div>
-  )
+  );
 }
 
-export default Messages
+export default Messages;
